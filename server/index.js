b5aff9d4133d75174b0bedcb9a5122351244a30e
Lunix <ramezjoseph8@gmail.com>
2023-05-30 19:06:44 +0200

    express server initiated

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+require('dotenv').config()
+
+// Middleware 
+app.use(cors())
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res
+    .status(200)
+    .json({
+        ok:true,
+        msg:'Hello from Express ✨'
+    })
+})
+
+// Run Server
+app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server running on ${process.env.PORT}`);
+})
