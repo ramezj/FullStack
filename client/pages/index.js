a43ea608ac3cbdea7e74a0841938f85e21aa5db2
Lunix <ramezjoseph8@gmail.com>
2023-05-31 08:55:15 +0200

    test

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,21 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [ data, setData ] = useState() 
+  useEffect(() => {
+    const testBackend = async () => {
+      const resp = await fetch(`${process.env.API_URL}/api/test`);
+      const response = await resp.json();
+      setData(response);
+    }
+    testBackend();
+  }, [])
   return (
     <div className='bg-[url("/6.jpg")] h-full min-h-screen bg-center bg-no-repeat w-full'>
+      {JSON.stringify(data)}
     </div>
   )
 }
